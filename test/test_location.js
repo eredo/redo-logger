1589bfbde1c58451a14afdae7778fa8dac7cc5dc
Eric Schneller <eric@schnellers.name>
2014-02-15 16:52:02 -0800

    - added location support - added basic log record handler implementation (NOTICE: This is just provisionally)

---
+++ test/test_location.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Logger = require('../lib/logger'),
+    expect = require('chai').expect;
+
+describe('logger stack location', function() {
+  var loc = {}, logger = Logger.getLogger('test');
+
+  before(function(done) {
+    Logger.setGlobalLevel(Logger.Level.FINE, true);
+    Logger.recordHandler.push(function(record) {
+      loc = record.location;
+    });
+    done();
+  });
+
+  it('should print have location col: 10, row: 5', function() {
+    logger.fine('Test', 'message');
+    expect(loc).to.be.a('object');
+    expect(loc).to.deep.equal({row: 18, col: 12, file: __dirname + '/test_location.js'});
+  });
+});
